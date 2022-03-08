bf862ef7ce5af297bc20f5346489fa6867375963
Caroline Arnold <arnold@dkrz.de>
2022-03-08 11:21:20 +0100

    Added new main program for MPI

--- first_demo/main_mpi.f90
+++ first_demo/main_mpi.f90
@@ -1,6 +1,7 @@
 PROGRAM routine
 
   USE, INTRINSIC :: iso_c_binding ! for cffi
+  USE mpi
 
   USE mo_type, ONLY: t_scalar_field_1d, t_scalar_field_2d
   USE mo_function, ONLY: f_scalar_field_1d, f_linspace, &
@@ -58,6 +59,11 @@ PROGRAM routine
   REAL           :: crate
   INTEGER        :: repeats
 
+  ! FORTRAN mpi side
+  INTEGER(KIND=4) :: error
+  INTEGER(KIND=4) :: mpi_rank
+  INTEGER(KIND=4) :: mpi_size
+
   ! Derived types
   TYPE(t_scalar_field_1d) :: sf_1d_fo ! fortran
   TYPE(t_scalar_field_1d) :: sf_1d_py ! python
@@ -79,11 +85,11 @@ PROGRAM routine
   CALL i_hello_world()
 
   ! (2) set default values
-  nx1 = 100
+  nx1 = 10000
   x1min = -5.0
   x1max = +5.0
 
-  repeats = 1
+  repeats = 3
  
   ! test for the interface
   PRINT *, 'shape in fortran', nx1
@@ -144,6 +150,92 @@ PROGRAM routine
   DEALLOCATE(sf_1d_fo % phi)
   DEALLOCATE(sf_1d_py % x)
   DEALLOCATE(sf_1d_py % phi)
+
+  ! ------------------------------------------------------------
+  !
+  ! Demo: 1D scalar field, using MPI
+  !
+  ! ------------------------------------------------------------
+   
+  PRINT *, '--------------------------------------------'
+  PRINT *, '       DEMO 1D Scalar Field  -- MPI         '
+  PRINT *, '--------------------------------------------'
+
+  CALL MPI_Init(error)
+  CALL MPI_Comm_rank(MPI_COMM_WORLD, mpi_rank, error);
+  CALL MPI_Comm_size(MPI_COMM_WORLD, mpi_size, error);
+  
+  ! (1) test for the interface
+  CALL i_hello_world()
+
+  ! (2) set default values
+  nx1 = 10000
+  x1min = -5.0
+  x1max = +5.0
+
+  repeats = 3
+ 
+  ! test for the interface
+  PRINT *, 'shape in fortran', nx1
+  CALL i_print_shape(nx1)
+  CALL i_print_value(x1min)
+
+  ! (3) initialize the arrays
+  sf_1d_fo % k = nx1
+
+  ! allocate them here
+  ! later we want to have allocated arrays to
+  ! share with the python routines
+  ALLOCATE(sf_1d_fo % x(nx1))
+  ALLOCATE(sf_1d_fo % phi(nx1))
+  ALLOCATE(sf_1d_py % x(nx1))
+  ALLOCATE(sf_1d_py % phi(nx1))
+
+  CALL f_linspace(nx1, x1min, x1max, sf_1d_fo % x)
+  CALL f_linspace(nx1, x1min, x1max, sf_1d_py % x)
+
+  ! (4) calculate the field value by a function
+  ! (4A) Fortran implementation
+  CALL system_clock(count=ic1, count_rate=crate, count_max=cmax)
+  DO i=1, repeats
+    CALL f_scalar_field_1d(nx1, sf_1d_fo % x, sf_1d_fo % phi)
+  END DO
+  CALL system_clock(count=ic2, count_rate=crate, count_max=cmax)
+
+  PRINT *, 'Time per Fortran function call (seconds)', (ic2 - ic1) /crate  / repeats
+
+  ! (4B) Python implementation
+  sf_1d_py % phi(:) = 0.0 ! initialize to zero
+  CALL system_clock(count=ic1, count_rate=crate, count_max=cmax)
+  DO i=1, repeats
+    CALL i_scalar_field_1d(nx1, sf_1d_py % x, sf_1d_py % phi)
+  END DO
+  CALL system_clock(count=ic2, count_rate=crate, count_max=cmax)
+
+  PRINT *, 'Time per Python  function call (seconds)', (ic2 - ic1) /crate  / repeats
+
+  ! (5) print to stdout
+  PRINT *, '   1D scalar field (head)'
+  PRINT *, '     (X)', '  Fortran (PHI)', '  Python (PHI)'
+  DO i=1, 10
+    PRINT *, sf_1d_fo % x(i), sf_1d_fo % phi(i), sf_1d_py % phi(i)
+  END DO
+
+  ! (6) check the arrays are equal
+  IF(ALL(sf_1d_fo % phi .EQ. sf_1d_py % phi)) THEN
+    PRINT *, ' Fortran and Python scalar field are equal'
+  ELSE
+    PRINT *, ' Fortran and Python scalar field are **NOT** equal'
+  ENDIF
+
+
+  ! (7) deallocate
+  DEALLOCATE(sf_1d_fo % x)
+  DEALLOCATE(sf_1d_fo % phi)
+  DEALLOCATE(sf_1d_py % x)
+  DEALLOCATE(sf_1d_py % phi)
+
+  CALL MPI_Finalize(error)
 
   ! ------------------------------------------------------------
   !
