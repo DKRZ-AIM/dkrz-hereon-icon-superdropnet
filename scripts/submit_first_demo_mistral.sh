4201b044c541989918d21e50b382fb358ae2bc0c
Caroline Arnold <arnold@dkrz.de>
2022-03-10 09:55:12 +0100

    Organized demo submit scripts in separater folder

--- scripts/submit_first_demo_mistral.sh
+++ scripts/submit_first_demo_mistral.sh
@@ -4,10 +4,10 @@
 #SBATCH --partition=compute
 #SBATCH --time=00:10:00
 #SBATCH --nodes=2
-#SBATCH --tasks-per-node=6
+#SBATCH --tasks-per-node=3
 
 module load intel intelmpi
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/first_demo"
 
-mpirun -np 12 ./my_demo_mpi
+map --profile srun ./my_demo_mpi
