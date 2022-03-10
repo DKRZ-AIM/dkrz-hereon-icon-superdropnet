cbdb76711a2ade1c73ad8a04f049e37087a83bda
Caroline Arnold <arnold@dkrz.de>
2022-03-10 15:42:41 +0100

    Makefile update for profiler

--- scripts/submit_first_demo_mistral.sh
+++ scripts/submit_first_demo_mistral.sh
@@ -7,10 +7,13 @@
 #SBATCH --tasks-per-node=3
 
 module load intel intelmpi
+module load arm-forge # for MAP source-level profiler
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/first_demo"
+
+cwd=${PWD}
 
 # change this to your git directory
 cd /work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/first_demo/
 
-map --profile srun ./my_demo_mpi
+map --profile -o $cwd/map_profile.map srun ./my_demo_mpi
