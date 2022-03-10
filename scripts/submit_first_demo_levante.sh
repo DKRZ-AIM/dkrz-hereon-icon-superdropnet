6a634b09db6ed5fdab272c383fabc97717d1b64b
Caroline Arnold <arnold@dkrz.de>
2022-03-10 09:57:30 +0100

    Fixed path

--- scripts/submit_first_demo_levante.sh
+++ scripts/submit_first_demo_levante.sh
@@ -16,4 +16,7 @@ ulimit -c 0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/home/k/k202141/first_steps_levante/2022-03-hereon-python-fortran-bridges/first_demo/"
 export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
 
+# change this to your git directory
+cd /work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/first_demo/
+
 srun -l --cpu_bind=verbose --hint=nomultithread --distribution=block:cyclic ./my_demo_mpi
--- scripts/submit_first_demo_mistral.sh
+++ scripts/submit_first_demo_mistral.sh
@@ -10,4 +10,7 @@ module load intel intelmpi
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/first_demo"
 
+# change this to your git directory
+cd /work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/first_demo/
+
 map --profile srun ./my_demo_mpi
