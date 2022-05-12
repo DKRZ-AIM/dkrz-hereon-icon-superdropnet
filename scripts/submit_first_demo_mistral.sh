49cbeefc5a6a95dbc22a5f755320c1f45fd7703b
Caroline Arnold <arnold@dkrz.de>
2022-05-12 11:53:02 +0200

    Removed map profile from srun to fix bbug issue #59

--- scripts/submit_first_demo_mistral.sh
+++ scripts/submit_first_demo_mistral.sh
@@ -16,4 +16,5 @@ cwd=${PWD}
 # change this to your git directory
 cd /work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/first_demo/
 
-map --profile -o $cwd/map_profile.map srun ./my_demo_mpi
+#map --profile -o $cwd/map_profile.map srun ./my_demo_mpi
+srun ./my_demo_mpi
