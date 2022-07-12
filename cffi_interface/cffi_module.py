49d48911a123eb397ac723d9ce677509d078ae3e
Caroline Arnold <arnold@dkrz.de>
2022-07-12 11:41:36 +0200

    Refactor code: loop on Fortran side, call with (qc, nc, qr, nr) directly

--- cffi_interface/cffi_module.py
+++ cffi_interface/cffi_module.py
@@ -92,35 +92,18 @@ def i_add_emi_echam_ttr(ptr_jg, ptr_jcs, ptr_jce,
     dxdt[jcs:jce][~cond] = 0.0
 
 @ffi.def_extern()
-def i_warm_rain_nn(ptr_ncells, ptr_nlevels, 
-                   ptr_istart, ptr_iend, ptr_kstart, ptr_kend,
-                   ptr_n_cloud_t0, ptr_q_cloud_t0,
-                   ptr_n_rain_t0,  ptr_q_rain_t0,  
-                   ptr_n_cloud_t1, ptr_q_cloud_t1, 
-                   ptr_n_rain_t1,  ptr_q_rain_t1,
-                   ptr_istate
-    ):
+def i_warm_rain_nn(ptr_n_moments, ptr_current_moments, ptr_new_moments,
+                   ptr_istate):
     """
     Call the pretrained warm rain network for inference for a given ikslice
     and time step. Calculates the new moments for cloud and rain.
 
     Parameters:
 
-    ptr_ncells : number of cells
-    ptr_nlevels : number of levels
-    ptr_istart : start of ikslice along cell dimension
-    ptr_iend    : end of ikslice along cell dimension
-    ptr_kstart  : start of ikslice along level dimension
-    ptr_kend    : end of ikslice along level dimension
-
-    ptr_n_cloud_t0 : cloud % n at previous time step
-    ptr_q_cloud_t0 : cloud % q at previous time step
-    ptr_n_rain_t0  : rain % n at previous time step
-    ptr_q_rain_t0  : rain % q at previous time step
-    ptr_n_cloud_t1 : cloud % n at next time step
-    ptr_q_cloud_t1 : cloud % q at next time step
-    ptr_n_rain_t1  : rain % n at next time step
-    ptr_q_rain_t1  : rain % q at next time step
+    ptr_n_moments       : number of moments (4 for warm rain)
+    ptr_current_moments : current moments in the order of 
+      cloud%q, cloud%n, rain%q, rain%n
+    ptr_new_moments     : new moments
 
     ptr_istate : flag to indicate state
        0 : nothing happened
@@ -128,40 +111,16 @@ def i_warm_rain_nn(ptr_ncells, ptr_nlevels,
        2 : ML inference update
        3 : error code (TODO)
 
-    Updates:
-    ptr_n_cloud_t1, ptr_q_cloud_t1, ptr_n_rain_t1, ptr_q_rain_t1, ptr_istate
     """
 
-    ncells = ptr_ncells[0]
-    nlevels = ptr_nlevels[0]
-    istart = ptr_istart[0]
-    iend = ptr_iend[0]
-    kstart = ptr_kstart[0]
-    kend = ptr_kend[0] 
-    istate = ptr_istate[0]
-    
-    # python indexing does not include last element
-    istart -= 1
-    kstart -= 1
+    n_moments = ptr_n_moments[0]
+    shape = (n_moments,)
 
-    shape = (ncells, nlevels)
-    n_cloud_t0 = transfer_arrays.asarray(ffi, ptr_n_cloud_t0, shape=shape)
-    q_cloud_t0 = transfer_arrays.asarray(ffi, ptr_q_cloud_t0, shape=shape)
-    n_rain_t0 = transfer_arrays.asarray(ffi, ptr_n_rain_t0, shape=shape)
-    q_rain_t0 = transfer_arrays.asarray(ffi, ptr_q_rain_t0, shape=shape)
+    current_moments = transfer_arrays.asarray(ffi, ptr_current_moments, shape=shape)
+    new_moments     = transfer_arrays.asarray(ffi, ptr_new_moments, shape=shape)
 
-    n_cloud_t1 = transfer_arrays.asarray(ffi, ptr_n_cloud_t1, shape=shape)
-    q_cloud_t1 = transfer_arrays.asarray(ffi, ptr_q_cloud_t1, shape=shape)
-    n_rain_t1 = transfer_arrays.asarray(ffi, ptr_n_rain_t1, shape=shape)
-    q_rain_t1 = transfer_arrays.asarray(ffi, ptr_q_rain_t1, shape=shape)
+    new_moments[:] = 0.0
     
-    n_cloud_t0 = n_cloud_t0[istart:iend, kstart:kend]
-    q_cloud_t0 = q_cloud_t0[istart:iend, kstart:kend]
-    q_rain_t0  = q_rain_t0[istart:iend, kstart:kend]
-    n_rain_t0  = n_rain_t0[istart:iend, kstart:kend]
-    
-    all_fortran_moments = np.stack((q_cloud_t0,n_cloud_t0,q_rain_t0,n_rain_t0),axis =-1)
-
     inputs_mean = np.asarray([[0.0002621447787797809, 51128093.51524663,
                     0.0003302890736022656, 5194.251154308974,
                     0.5566250557023539, 4.8690682855354596e-12,
@@ -187,76 +146,18 @@ def i_warm_rain_nn(ptr_ncells, ptr_nlevels,
     hard_coded_path = '/work/ka1176/caroline/gitlab/2022-03-hereon-python-fortran-bridges/cffi_interface'
     trained_model = pl_model.load_from_checkpoint(hard_coded_path + "/trained_models/best_model.ckpt")
 
-    q_cloud_t1[:,:] = 0.0
-    n_cloud_t1[:,:] = 0.0
-    q_rain_t1[:,:]  = 0.0
-    n_rain_t1[:,:]  = 0.0
+    # ML inference only if input moments are non zero
+    if np.all(current_moments == 0.0):
+        new_moments[:] = 0.0
+        ptr_istate[0] = 1
 
+    else:
+        #Solver class initalized and new moment calculated
+        new_forecast = simulation_forecast(
+          current_moments, trained_model,inputs_mean, inputs_std
+        )
+        new_forecast.test()
 
-    #Loop starts here 
-    for i in range (istart, iend):
-        for k in range (kstart, kend):
-            # indexing issue
-            # fortran_moments = all_fortran_moments[i,k - kstart,:]  
+        new_moments[:] = new_forecast.moments_out[0,:]
 
-            fortran_moments = all_fortran_moments[i, 0, :]
-
-            # ML inference only if input moments are non zero
-            if np.all(fortran_moments == 0.0):
-                q_cloud_t1[i,k] = 0.0
-                n_cloud_t1[i,k] = 0.0
-                q_rain_t1[i,k]  = 0.0
-                n_rain_t1[i,k]  = 0.0
-                ptr_istate[0] = 1
-
-            else:
-                #Solver class initalized and new moment calculated
-                new_forecast = simulation_forecast(
-            fortran_moments, trained_model,inputs_mean, inputs_std
-            )
-                new_forecast.test()
-
-                q_cloud_t1[i,k] = new_forecast.moments_out[0, 0]
-                n_cloud_t1[i,k] = new_forecast.moments_out[0, 1]
-                q_rain_t1[i,k]  = new_forecast.moments_out[0, 2]
-                n_rain_t1[i,k]  = new_forecast.moments_out[0, 3]
-                ptr_istate[0] = 2
-           
-           
-
-#@ffi.def_extern()
-#def i_print_shape(nx):
-#    print(" shape in python", nx, type(nx), nx[0])
-#
-#@ffi.def_extern()
-#def i_print_value(x):
-#    print(" value in python", x, type(x), x[0])
-#
-#@ffi.def_extern()
-#def i_print_gpu():
-#    if torch.cuda.is_available():
-#        print("Has GPU")
-#    else:
-#        print("Does not have GPU")
-#
-#@ffi.def_extern()
-#def i_scalar_field_1d(ptr_nx, ptr_x, ptr_phi):
-#    nx = ptr_nx[0] # TODO check this
-#    x = transfer_arrays.asarray(ffi, ptr_x, shape=(nx,))
-#    phi = transfer_arrays.asarray(ffi, ptr_phi, shape=(nx,))
-#
-#    # the brackets [:] are important here
-#    phi[:] = 0.5 * x**2
-#
-#    time.sleep(60)
-#
-#@ffi.def_extern()
-#def i_scalar_field_2d(ptr_nx1, ptr_nx2, ptr_x1, ptr_x2, ptr_phi):
-#    nx1 = ptr_nx1[0]
-#    nx2 = ptr_nx2[0]
-#    x1 = transfer_arrays.asarray(ffi, ptr_x1, shape=(nx1, nx2))
-#    x2 = transfer_arrays.asarray(ffi, ptr_x2, shape=(nx1, nx2))
-#    phi = transfer_arrays.asarray(ffi, ptr_phi, shape=(nx1, nx2))
-#
-#    # the brackets [:] are important here
-#    phi[:,:] = 0.5 * (x1 - 1.0)**2 + x2**2 + 2*x2
+        ptr_istate[0] = 2
